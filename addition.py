34856eace164f9c8fdc5a8e08f2e425bb3826d3f
thePOET8 <66527630+thePOET8@users.noreply.github.com>
2024-02-19 20:41:01 +0800

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,22 +1,24 @@
-#因为我想在复制时保留原网页的样式和颜色，所以目前还有直接打开的方式，后续会改进
+#因为我想保留原本的颜色样式，所以还是用了打开原网页，后续可能有改进
 import webbrowser
 import time
-
+from tkinter import Tk, simpledialog
 
 def search_word(word):
-    # 定义搜索的URL
     urls = [
         f"https://www.ldoceonline.com/dictionary/{word}",
         f"https://www.etymonline.com/word/{word}#etymonline_v_14014"
     ]
 
     for url in urls:
-        # 使用默认浏览器打开URL
         webbrowser.open(url)
-
-        # 在每个请求之间添加延时，以避免同时打开太多标签
         time.sleep(1)
 
+def get_word_from_user():
+    root = Tk()
+    root.withdraw()  # 不显示主窗口
+    word = simpledialog.askstring("输入", "请输入想搜索的单词:")
+    if word:
+        search_word(word)
+    root.destroy()
 
-# 测试函数
-search_word("flaw")
+get_word_from_user()
